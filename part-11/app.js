72fc6119f66aa1a2c43472cfeccd76bf3d6b9055
190031222 <190031222@kluniversity.in>
2021-07-05 10:23:18 +0530

    part-11

---
+++ part-11/app.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express()
+
+// Heroku dynamically sets a port
+const PORT = process.env.PORT || 5000
+
+app.use(express.static('dist'))
+
+app.get('/health', (_, res) => {
+  res.send('ok!')
+})
+
+app.listen(PORT, () => {
+  console.log(`server started on port ${PORT}`)
+})
